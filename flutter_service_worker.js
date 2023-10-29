de4c6935480d015187d4585021162d2791858cd7
oofp <oofpgit@gmail.com>
2023-10-28 23:33:57 -0400

    add province input

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -18,9 +18,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "f78786b349c3c0382c21c8c50f0a7256",
-"/": "f78786b349c3c0382c21c8c50f0a7256",
-"main.dart.js": "30f940ebd44a9e3cdf069d0f8f3d5167",
+"index.html": "9f170556f61771b1305af71d3b01022c",
+"/": "9f170556f61771b1305af71d3b01022c",
+"main.dart.js": "686dcd3c71179dceff9e1cf067557e3e",
 "manifest.json": "071d9be92f6deab864afb09f94eff8a3",
 "version.json": "7b195e1a1366dede5c0a15d457b37a84"
 };
