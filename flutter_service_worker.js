9408292878bda2333582159ecabe46f1ee7eec93
oofp <oofpgit@gmail.com>
2023-10-30 20:05:50 -0400

    improve charts

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,9 +23,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "ecdbfb76be5d5a80ab6446ce1de7b72c",
-"/": "ecdbfb76be5d5a80ab6446ce1de7b72c",
-"main.dart.js": "9a999895808156d19604020d8d780dd9",
+"index.html": "ba4ccaeff4df3a3884d4aff2289d44c2",
+"/": "ba4ccaeff4df3a3884d4aff2289d44c2",
+"main.dart.js": "577bda065e2bc6f10d4dfb7e5f5b31fc",
 "manifest.json": "071d9be92f6deab864afb09f94eff8a3",
 "version.json": "7b195e1a1366dede5c0a15d457b37a84"};
 // The application shell files that are downloaded before a service worker can
