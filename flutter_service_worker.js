53dc6ee7c9122f3a522f5149eec021682ec5f2d8
oofp <oofpgit@gmail.com>
2023-11-04 23:06:42 -0400

    add balances chart

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,9 +23,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "e9bf9384c7d8f5670d74f220b933e4c0",
-"/": "e9bf9384c7d8f5670d74f220b933e4c0",
-"main.dart.js": "4766c99cf292d9e4d3f63c96604d7db8",
+"index.html": "a94b7da4dd86fba2248c295c36e6f6f9",
+"/": "a94b7da4dd86fba2248c295c36e6f6f9",
+"main.dart.js": "6811a7b3cc8ea278c6edc7afb01624bf",
 "manifest.json": "071d9be92f6deab864afb09f94eff8a3",
 "version.json": "7b195e1a1366dede5c0a15d457b37a84"};
 // The application shell files that are downloaded before a service worker can
