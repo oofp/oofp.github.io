76753940295df9f7e3ee02b044d7939666d01175
oofp <oofpgit@gmail.com>
2023-11-09 19:53:24 -0500

    fix typo at about screen and add scrolling using mouse

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,9 +23,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "9bba8ee619ab79c8acdf367e5f6003c9",
-"/": "9bba8ee619ab79c8acdf367e5f6003c9",
-"main.dart.js": "f3832ce6e2a442542c579b0a4344a7dd",
+"index.html": "94463b7aaf1cc28b62d2b25543509091",
+"/": "94463b7aaf1cc28b62d2b25543509091",
+"main.dart.js": "7df5356c560175195d979ad0f8228180",
 "manifest.json": "071d9be92f6deab864afb09f94eff8a3",
 "version.json": "7b195e1a1366dede5c0a15d457b37a84"};
 // The application shell files that are downloaded before a service worker can
