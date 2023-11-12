2fec6757b52d34911903d536c24c9c048e1c96f2
oofp <oofpgit@gmail.com>
2023-11-12 11:07:43 -0500

    improve simulation

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,9 +23,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "45894d74321c9fb646935750c89f2d63",
-"/": "45894d74321c9fb646935750c89f2d63",
-"main.dart.js": "00037e4532f030e9d747765deef8a2c3",
+"index.html": "9cd55d48747a884e4b02310ed3776e77",
+"/": "9cd55d48747a884e4b02310ed3776e77",
+"main.dart.js": "1500b94208cf6d097c74f8f9b993a702",
 "manifest.json": "071d9be92f6deab864afb09f94eff8a3",
 "version.json": "7b195e1a1366dede5c0a15d457b37a84"};
 // The application shell files that are downloaded before a service worker can
