79f11a2a49a1fc137b5d4152c526853aae5fc6d0
oofp <oofpgit@gmail.com>
2024-01-06 16:01:04 -0500

    support retired already; use taxes/credits for 2024; important fixes including tax computation

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,9 +23,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "615ac4b08afed771e62ecab93411d7b7",
-"/": "615ac4b08afed771e62ecab93411d7b7",
-"main.dart.js": "d6f891d3b05fea23345aaa2dacfada6b",
+"index.html": "4d5e5080b9a28fa23e5eb47934c8ad39",
+"/": "4d5e5080b9a28fa23e5eb47934c8ad39",
+"main.dart.js": "d1224ecdfe79654761ba9765edd30817",
 "manifest.json": "071d9be92f6deab864afb09f94eff8a3",
 "version.json": "7b195e1a1366dede5c0a15d457b37a84"};
 // The application shell files that are downloaded before a service worker can
