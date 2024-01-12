fc7e07bf3caf94717764fc401e8dfeb3308fde90
oofp <oofpgit@gmail.com>
2024-01-11 20:47:05 -0500

    support variable withdrwal income strategy

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,9 +23,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "245450aac223e57eb95e196ea8f7d7a4",
-"/": "245450aac223e57eb95e196ea8f7d7a4",
-"main.dart.js": "d7147c033461321282cd3e08a8bdcebe",
+"index.html": "d811c88a33a9d29f6c3d97679c2742fa",
+"/": "d811c88a33a9d29f6c3d97679c2742fa",
+"main.dart.js": "7c864cba57a3ed58446b5edfb81f5e03",
 "manifest.json": "071d9be92f6deab864afb09f94eff8a3",
 "version.json": "7b195e1a1366dede5c0a15d457b37a84"};
 // The application shell files that are downloaded before a service worker can
