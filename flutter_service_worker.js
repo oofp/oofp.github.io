9940140e8c65ec5e68dcf34d940c042c1458e6c1
oofp <oofpgit@gmail.com>
2024-01-17 08:42:22 -0500

    Use material3 and segmented buttons

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,9 +23,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "40882667d741fab1eb66a4ec8bb5cfbd",
-"/": "40882667d741fab1eb66a4ec8bb5cfbd",
-"main.dart.js": "8adae2287fccf089e3cfeae775343efe",
+"index.html": "9a0860b7dd2e3ccfb5c9e8090f65b149",
+"/": "9a0860b7dd2e3ccfb5c9e8090f65b149",
+"main.dart.js": "c470b6012348eaa79b6b046e3afcd343",
 "manifest.json": "071d9be92f6deab864afb09f94eff8a3",
 "version.json": "7b195e1a1366dede5c0a15d457b37a84"};
 // The application shell files that are downloaded before a service worker can
