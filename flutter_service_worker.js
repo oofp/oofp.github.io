d4eefd50e47c47958a9d1ef85bff3f23bb8887d3
oofp <oofpgit@gmail.com>
2024-01-27 21:41:14 -0500

    support additional withdrawals and improvements for trackbal behavior for some charts

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -6,8 +6,8 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"assets/fonts/MaterialIcons-Regular.otf": "ece3ac22795aff43f099ad75cce94245",
-"assets/NOTICES": "60e8a4cd446885c09731922ad3244228",
+"assets/fonts/MaterialIcons-Regular.otf": "05c1dfc7f552b6bec9de2257368de2fb",
+"assets/NOTICES": "66bfebf69961f120355929b9dc5177ee",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "89ed8f4e49bcdfc0b5bfc9b24591e347",
 "assets/shaders/ink_sparkle.frag": "f8b80e740d33eb157090be4e995febdf",
 "canvaskit/canvaskit.js": "bbf39143dfd758d8d847453b120c8ebb",
@@ -23,9 +23,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "20c3575bd60d2c7b53d343016b6a0c90",
-"/": "20c3575bd60d2c7b53d343016b6a0c90",
-"main.dart.js": "0357f2879947553febd9774b1fa6b5a4",
+"index.html": "14b463568c9f3fb190537295a827eeee",
+"/": "14b463568c9f3fb190537295a827eeee",
+"main.dart.js": "6d02797bd275af729f0384e9b01dd890",
 "manifest.json": "071d9be92f6deab864afb09f94eff8a3",
 "version.json": "7b195e1a1366dede5c0a15d457b37a84"};
 // The application shell files that are downloaded before a service worker can
