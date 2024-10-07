1be3b37dc7e0e326fd6c7ae3376fc5a5193affba
oofp <oofpgit@gmail.com>
2024-10-07 00:03:28 -0400

    Fix OAS clawback computation and income tax computation; OAS clawback is based on previous year income and more minor fixes

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -31,7 +31,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "bde06b9298b4254d3c24b51cb75fb5d8
 "icons/Icon-maskable-512.png": "21a471421708230a32d65914dadbe311",
 "index.html": "5f601b5a190e85acc1efba806b54ccfd",
 "/": "5f601b5a190e85acc1efba806b54ccfd",
-"main.dart.js": "8fdbbf0536f1058f93b61d8e5b4a3b2e",
+"main.dart.js": "e882c6fd461a690204e66ade7f3c282d",
 "manifest.json": "071d9be92f6deab864afb09f94eff8a3",
 "version.json": "7b195e1a1366dede5c0a15d457b37a84"};
 // The application shell files that are downloaded before a service worker can
