8b6e796d30054544c0c376ba69f05aa6fa9f2b8a
oofp <oofpgit@gmail.com>
2024-10-09 13:21:40 -0400

    Fix help info and percentils at portfolioProjections diagram

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -31,7 +31,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "bde06b9298b4254d3c24b51cb75fb5d8
 "icons/Icon-maskable-512.png": "21a471421708230a32d65914dadbe311",
 "index.html": "5f601b5a190e85acc1efba806b54ccfd",
 "/": "5f601b5a190e85acc1efba806b54ccfd",
-"main.dart.js": "e882c6fd461a690204e66ade7f3c282d",
+"main.dart.js": "69fda33405da1bc82fb8f8d0823ae681",
 "manifest.json": "071d9be92f6deab864afb09f94eff8a3",
 "version.json": "7b195e1a1366dede5c0a15d457b37a84"};
 // The application shell files that are downloaded before a service worker can
