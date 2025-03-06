29d54b8a3ee915b8648d4f1bafedfa094e5159cd
oofp <oofpgit@gmail.com>
2025-03-05 23:44:34 -0500

    Decrease number of simulations to 100

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2089959282"
+    serviceWorkerVersion: "2027082969"
   }
 });
