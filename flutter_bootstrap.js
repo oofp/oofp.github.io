cb286054f34ed2134b17aad91f6d8def43c74763
oofp <oofpgit@gmail.com>
2025-04-19 13:50:54 -0400

    fixes at sf data table and color of eveluation table header

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4040519706"
+    serviceWorkerVersion: "3048503593"
   }
 });
