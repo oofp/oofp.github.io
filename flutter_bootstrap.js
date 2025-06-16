616855ba3d9eed77a1a6fd29b035ed49aba0e4ce
oofp <oofpgit@gmail.com>
2025-06-15 22:29:29 -0400

    fix issue with negative rrsp balance

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1683158148"
+    serviceWorkerVersion: "2743001002"
   }
 });
