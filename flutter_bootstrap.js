01d198e1e842b5a5bfdc66bc9bd62f29eaf0ac76
oofp <oofpgit@gmail.com>
2025-07-08 08:43:12 -0400

    Fix typo 'retirerment'

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "402417038"
+    serviceWorkerVersion: "1596576921"
   }
 });
