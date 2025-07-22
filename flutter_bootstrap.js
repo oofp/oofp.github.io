e0a7fe3af94acd4b334f68c3da461cdd618c9243
oofp <oofpgit@gmail.com>
2025-07-22 12:09:57 -0400

    fix minimal withdrawal enforcing when RRSP meltdown is disabled

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "690818529"
+    serviceWorkerVersion: "1444381236"
   }
 });
