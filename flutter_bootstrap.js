00b14a6ad83f764412db0b10c1bcccfd02206a2a
oofp <oofpgit@gmail.com>
2025-08-18 18:14:55 -0400

    InputData refactoring

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3098349673"
+    serviceWorkerVersion: "3261474961"
   }
 });
