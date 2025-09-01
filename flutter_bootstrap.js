869cb3be53c9f715afd94e05e731f51ac3e95dea
oofp <oofpgit@gmail.com>
2025-09-01 15:29:52 -0400

    improve header when exporting to excel

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1620950893"
+    serviceWorkerVersion: "1872935985"
   }
 });
