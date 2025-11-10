8ddbe1b440f9c7d7e4a2ccce2a85979688936a4c
oofp <oofpgit@gmail.com>
2025-11-10 02:15:42 -0500

    apply configured inflation for asset allocation

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2641917347"
+    serviceWorkerVersion: "3170415540"
   }
 });
