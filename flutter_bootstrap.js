03ecc879359e43d5b6371a4def51bbb8c7e966a3
oofp <oofpgit@gmail.com>
2025-11-15 18:34:10 -0500

    support RRSP tax cost limit as part of RRSP withdrawal strategy

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3339190001"
+    serviceWorkerVersion: "2443937121"
   }
 });
