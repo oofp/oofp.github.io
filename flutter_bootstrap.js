9329408433e197b04fa87e5ef26a5397b9a38ab4
oofp <oofpgit@gmail.com>
2025-11-19 09:37:21 -0500

    another effective tax refinement

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2024406261"
+    serviceWorkerVersion: "1047553922"
   }
 });
