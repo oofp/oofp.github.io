943e2d044939f20cb3517e3599c18952a2044527
oofp <oofpgit@gmail.com>
2025-12-31 16:57:09 -0500

    fix issue with tall image when generating pdf and improve oom error msg when generating snapshot

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "706070997"
+    serviceWorkerVersion: "3822002812"
   }
 });
