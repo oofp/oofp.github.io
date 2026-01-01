552b47539a675ce44fe2cb503bd1baa8590206c5
oofp <oofpgit@gmail.com>
2026-01-01 01:53:42 -0500

    using GIS lookup table to determine GIS

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3822002812"
+    serviceWorkerVersion: "544349569"
   }
 });
