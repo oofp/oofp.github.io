473d7f8f63b065f47b042d5cc0176cb84320783b
oofp <oofpgit@gmail.com>
2026-01-04 16:07:12 -0500

    fix issue with strategy table when there are failed strategies; Add. Income collected at ComparePlans

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "474736470"
+    serviceWorkerVersion: "1094726049"
   }
 });
